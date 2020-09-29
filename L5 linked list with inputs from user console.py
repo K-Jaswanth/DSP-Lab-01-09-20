82c184b890fd8da6054a3c42622a530d4a4a9e36
K-Jaswanth <70188692+K-Jaswanth@users.noreply.github.com>
2020-09-29 10:55:36 +0530

    Add files via upload

---
+++ L5 linked list with inputs from user console.py
@@ -0,0 +1,14 @@
+# linked list with inputs from user console
+lst = [] 
+
+# number of elemetns as input 
+n = int(input("Enter number of elements : ")) 
+
+# iterating till the range 
+for i in range(0, n): 
+	ele = int(input()) 
+	lst.append(ele)
+
+	
+# adding the element 
+print(lst) 
